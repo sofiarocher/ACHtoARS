16105c6d5d10065aa10843714bbc290182650dbe
Sofía Rocher <76635239+sofiarocher@users.noreply.github.com>
2025-01-29 01:49:39 -0300

    added analytics from vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Limelight } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,6 +35,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${limelight.variable} antialiased`}
       >
+        <Analytics />
         {children}
       </body>
     </html>
