e36649d188d45fb2ad388edad1c0b002c90ba5ba
Sofía Rocher <76635239+sofiarocher@users.noreply.github.com>
2025-02-12 02:06:04 -0300

    Update branch.tsx

--- components/layouts/branch.tsx
+++ components/layouts/branch.tsx
@@ -3,16 +3,20 @@ import { ExternalLink } from "@/components/ui/link";
 
 export default function Branch() {
   return (
-    <ExternalLink 
-      href="https://github.com/srocher-dev/ach-to-ars-calculator"
+    <ExternalLink
+      href="https://github.com/sofiarocher/ACHtoARS"
       text={
-        <div className="group fixed bottom-4 right-4 flex items-center bg-white rounded-full 
-          hover:pr-4 transition-all duration-300 ease-in-out border border-black/20">
+        <div
+          className="group fixed bottom-16 right-4 flex items-center bg-green sm:bg-green/40 rounded-full 
+          hover:pr-4 transition-all duration-500 ease-in-out border border-gray sm:text-sm text-xs font-light"
+        >
           <div className="p-2">
-            <GitBranch className="w-4 h-4 text-black" />
+            <GitBranch className="w-4 h-4 text-gray" />
           </div>
-          <span className="max-w-0 overflow-hidden whitespace-nowrap group-hover:max-w-[100px] 
-            transition-all duration-300 ease-in-out text-black">
+          <span
+            className="max-w-0 overflow-hidden whitespace-nowrap group-hover:max-w-[245px] 
+            transition-all duration-300 ease-in-out text-gray"
+          >
             Contribute 💻
           </span>
         </div>
